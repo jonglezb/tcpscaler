6b370e095b24a394e1bc53ccf05c99c3b5ac81d8
Baptiste Jonglez <baptiste.jonglez@imag.fr>
2017-04-18 18:37:36 +0200

    server: set fs.file-max to a higher value

--- setup-server.sh
+++ setup-server.sh
@@ -3,6 +3,7 @@
 
 sysctl net.ipv4.tcp_syncookies=0
 sysctl net.core.somaxconn=8192
+sysctl fs.file-max=12582912
 
 iptables -t raw -A PREROUTING -p tcp -j NOTRACK
 iptables -t raw -A OUTPUT -p tcp -j NOTRACK
