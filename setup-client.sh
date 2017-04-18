ea74fa5a995677aa09204fed77eaf609c5e03733
Baptiste Jonglez <baptiste.jonglez@imag.fr>
2017-04-18 15:54:55 +0200

    Setup scripts: remove now useless ulimit, and add missing iptables rule

--- setup-client.sh
+++ setup-client.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-
 # Should be run as root
 
-ulimit -n 65536
-
 sysctl net.ipv4.ip_local_port_range="1024 65535"
-
 sysctl net.ipv4.tcp_tw_reuse=1
 
+iptables -t raw -A PREROUTING -p tcp -j NOTRACK
 iptables -t raw -A OUTPUT -p tcp -j NOTRACK
