4d3b409e988f5ae779a87bda353912e89be86f50
Baptiste Jonglez <baptiste.jonglez@imag.fr>
2018-04-27 18:18:11 +0200

    Move general utilities function to a separate header

--- common.h
+++ common.h
@@ -1,3 +1,5 @@
+#include "utils.h"
+
 /* Maximum expected response time for a query.  This is used to compute
    how many queries in flight we should expect on each connection, and
    thus how many timestamps to keep in memory. */
@@ -16,32 +18,6 @@
 /* How many commands we are prepared to accept on stdin. */
 #define MAX_STDIN_COMMANDS 256
 
-/* Copied from babeld by Juliusz Chroboczek */
-#define DO_NTOHS(_d, _s) \
-    do { unsigned short _dd; \
-         memcpy(&(_dd), (_s), 2); \
-         _d = ntohs(_dd); } while(0)
-#define DO_NTOHL(_d, _s) \
-    do { unsigned int _dd; \
-         memcpy(&(_dd), (_s), 4); \
-         _d = ntohl(_dd); } while(0)
-#define DO_HTONS(_d, _s) \
-    do { unsigned short _dd; \
-         _dd = htons(_s); \
-         memcpy((_d), &(_dd), 2); } while(0)
-#define DO_HTONL(_d, _s) \
-    do { unsigned _dd; \
-         _dd = htonl(_s); \
-         memcpy((_d), &(_dd), 4); } while(0)
-
-#define error(...) \
-            do { fprintf(stderr, __VA_ARGS__); } while (0)
-
-#define info(...) \
-            do { if (verbose >= 1) fprintf(stderr, __VA_ARGS__); } while (0)
-
-#define debug(...) \
-            do { if (verbose >= 2) fprintf(stderr, __VA_ARGS__); } while (0)
 
 
 static short verbose;
@@ -117,30 +93,4 @@ void generate_poisson_interarrival(struct timeval* tv, double rate)
   double interarrival = - log(1. - u) / rate;
   tv->tv_sec = (time_t) floor(interarrival);
   tv->tv_usec = lrint(interarrival * 1000000.) % 1000000;
-}
-
-void subtract_timespec(struct timespec *result, const struct timespec *a, const struct timespec *b)
-{
-  if ((a->tv_sec < b->tv_sec) ||
-      ((a->tv_sec == b->tv_sec) &&
-       (a->tv_nsec <= b->tv_nsec))) {		/* TIME1 <= TIME2? */
-    result->tv_sec = result->tv_nsec = 0 ;
-  } else {						/* TIME1 > TIME2 */
-    result->tv_sec = a->tv_sec - b->tv_sec ;
-    if (a->tv_nsec < b->tv_nsec) {
-      result->tv_nsec = a->tv_nsec + 1000000000L - b->tv_nsec ;
-      result->tv_sec-- ;				/* Borrow a second. */
-    } else {
-      result->tv_nsec = a->tv_nsec - b->tv_nsec ;
-    }
-  }
-}
-
-void timeval_add_ms(struct timeval *a, unsigned int ms)
-{
-  a->tv_usec += ms * 1000;
-  while (a->tv_usec >= 1000000L) {
-    a->tv_sec += 1;
-    a->tv_usec -= 1000000L;
-  }
 }
---
+++ utils.h
@@ -0,0 +1,54 @@
+#include <time.h>
+
+/* Copied from babeld by Juliusz Chroboczek */
+#define DO_NTOHS(_d, _s) \
+    do { unsigned short _dd; \
+         memcpy(&(_dd), (_s), 2); \
+         _d = ntohs(_dd); } while(0)
+#define DO_NTOHL(_d, _s) \
+    do { unsigned int _dd; \
+         memcpy(&(_dd), (_s), 4); \
+         _d = ntohl(_dd); } while(0)
+#define DO_HTONS(_d, _s) \
+    do { unsigned short _dd; \
+         _dd = htons(_s); \
+         memcpy((_d), &(_dd), 2); } while(0)
+#define DO_HTONL(_d, _s) \
+    do { unsigned _dd; \
+         _dd = htonl(_s); \
+         memcpy((_d), &(_dd), 4); } while(0)
+
+#define error(...) \
+            do { fprintf(stderr, __VA_ARGS__); } while (0)
+
+#define info(...) \
+            do { if (verbose >= 1) fprintf(stderr, __VA_ARGS__); } while (0)
+
+#define debug(...) \
+            do { if (verbose >= 2) fprintf(stderr, __VA_ARGS__); } while (0)
+
+void subtract_timespec(struct timespec *result, const struct timespec *a, const struct timespec *b)
+{
+  if ((a->tv_sec < b->tv_sec) ||
+      ((a->tv_sec == b->tv_sec) &&
+       (a->tv_nsec <= b->tv_nsec))) {		/* TIME1 <= TIME2? */
+    result->tv_sec = result->tv_nsec = 0 ;
+  } else {						/* TIME1 > TIME2 */
+    result->tv_sec = a->tv_sec - b->tv_sec ;
+    if (a->tv_nsec < b->tv_nsec) {
+      result->tv_nsec = a->tv_nsec + 1000000000L - b->tv_nsec ;
+      result->tv_sec-- ;				/* Borrow a second. */
+    } else {
+      result->tv_nsec = a->tv_nsec - b->tv_nsec ;
+    }
+  }
+}
+
+void timeval_add_ms(struct timeval *a, unsigned int ms)
+{
+  a->tv_usec += ms * 1000;
+  while (a->tv_usec >= 1000000L) {
+    a->tv_sec += 1;
+    a->tv_usec -= 1000000L;
+  }
+}
